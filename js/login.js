5e224520c932d479ec9248f3aa460b5130242c70
bryce <a0955361101@gmail.com>
2023-11-17 16:26:29 +0800

    feat: added register

--- js/login.js
+++ js/login.js
@@ -1,7 +1,26 @@
 const userNameLabel = document.querySelector('.user_name_label');
 const userPasswordLabel = document.querySelector('.user_password_label');
+const nameLabel = document.querySelector('.name_label');
 const userNameInput = document.querySelector('.username_input');
 const userPasswordInput = document.querySelector('.user_password_input');
+const registerBtn = document.querySelector('.register_btn');
+const loginBtn = document.querySelector('.login_btn');
+const nameBox = document.querySelector('.name');
+const title = document.querySelector('.title');
+let page = 0; // 預設 0 登入頁 1註冊頁
+let pageTitle = 'Login';
+
+title.innerHTML = pageTitle;
+
+if(page === 1){
+    registerBtn.style.display = 'none';
+    loginBtn.style.display = 'block';
+}
+else if(page === 0){
+    loginBtn.style.display = 'none';
+    registerBtn.style.display = 'block';
+    nameBox.style.display = 'none';
+}
 
 const focusUserInput = (value) => {
     if(value === 'userName'){
@@ -13,6 +32,11 @@ const focusUserInput = (value) => {
         userPasswordLabel.style.top = '-50%';
         userPasswordLabel.style.fontSize = '12px';
         userPasswordLabel.style.color = 'rgb(66 184 131 / 1)';
+    }
+    else if(value === 'name'){
+        nameLabel.style.top = '-50%';
+        nameLabel.style.fontSize = '12px';
+        nameLabel.style.color = 'rgb(66 184 131 / 1)';
     }
 }
 
@@ -27,4 +51,26 @@ const blurUserInput = (value) => {
         userPasswordLabel.style.fontSize = '16px';
         userPasswordLabel.style.color = '#fff';
     }
+    else if(value === 'name'){
+        nameLabel.style.top = '25%';
+        nameLabel.style.fontSize = '16px';
+        nameLabel.style.color = '#fff';
+    }
+}
+
+const changeRegister = (number) => {
+    page = number;
+    if(page === 1){
+        registerBtn.style.display = 'none';
+        loginBtn.style.display = 'block';
+        nameBox.style.display = 'block';
+        pageTitle = 'Register';
+    }
+    else if(page === 0){
+        loginBtn.style.display = 'none';
+        registerBtn.style.display = 'block';
+        nameBox.style.display = 'none';
+        pageTitle = 'Login';
+    }
+    title.innerHTML = pageTitle;
 }
